8c5b9e64a9f0753d6dcf93c17d43aeeb6cd0ebf3
Erik Chen <ericisjustinbieber@gmail.com>
2022-04-24 01:31:31 +0800

    加入router.param to check ID (url)

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,6 +5,17 @@ const postsDataJson = fs.readFileSync('data/data.json', 'utf8', (err, data) => {
 });
 const postsData = JSON.parse(postsDataJson); //Array
 
+//middleware function
+exports.checkID = (req, res, next, value) => {
+  if (req.params.id * 1 > postsData.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
 // functions
 exports.getAllPosts = (req, res) => {
   console.log('received get posts request!👻');
@@ -14,16 +25,16 @@ exports.getAllPosts = (req, res) => {
   });
 };
 exports.getPersonalPost = (req, res) => {
-  const id = req.params.id * 1; // Number
-  const user = postsData[id - 1];
-  console.log(user);
-  console.log(`received get personal post request!👻 id: ${id}`);
-  if (!user) {
-    res.status(404).json({
-      status: 'fail',
-      message: 'Personal Post Not Found',
-    });
-  }
+  //   const id = req.params.id * 1; // Number
+  const post = postsData.find((el) => el.id === id);
+  //   console.log(user);
+  //   console.log(`received get personal post request!👻 id: ${id}`);
+  //   if (!post) {
+  //     res.status(404).json({
+  //       status: 'fail',
+  //       message: 'Personal Post Not Found',
+  //     });
+  //   }
   res.status(200).json({
     status: 'success',
     data: postsData[id - 1],
@@ -64,13 +75,13 @@ exports.updatePost = (req, res) => {
 exports.deletePost = (req, res) => {
   const id = req.params.id * 1;
   console.log(`delete post on ${id}`);
-  if (id > postsData.length) {
-    console.error('!!!!!!!!!!!!!!!!cannot delete!!!!!!!!!!!!!!');
-    return res.status(404).json({
-      status: 'fail',
-      message: 'post on id not found',
-    });
-  }
+  //   if (id > postsData.length) {
+  //     console.error('!!!!!!!!!!!!!!!!cannot delete!!!!!!!!!!!!!!');
+  //     return res.status(404).json({
+  //       status: 'fail',
+  //       message: 'post on id not found',
+  //     });
+  //   }
 
   res.status(204).json({
     status: 'success',
--- routes/postsRoute.js
+++ routes/postsRoute.js
@@ -4,6 +4,8 @@ const router = express.Router();
 // 從 controller 得到 對應要求的方法 GET POST PATCH DELETE
 const postController = require('../controllers/postController');
 
+router.param('id', postController.checkID);
+
 // get (客戶端得到訊息)
 router
   .route('/')
